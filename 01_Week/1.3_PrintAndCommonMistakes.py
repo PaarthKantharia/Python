505063e166e7e12d2e5e2e5129da4789815c35bb
Paarth Kantharia <paarth.kantharia0078@gmail.com>
2025-01-20 11:31:28 +0000

    Print & Common Mistakes Done

---
+++ 01_Week/1.3_PrintAndCommonMistakes.py
@@ -0,0 +1,18 @@
+# We can print strings with pirnt('') command -
+print('Hello India ')
+
+# We can even print multiple strings in the same print statemtent seperarted by a coma
+print('Hello India', 'Hello World', 'Hi Python students')
+
+# We can print numbers too 
+print('20')
+
+# We can print decimal/fractional numbers too
+print('20.5')
+
+# We can print strings, whole numbers , decimal/fractional numbers all in a single print command too seperated by a coma 
+print('Hello India', 10, 20.5)
+
+
+# Python is very strict with type of brackets as only round brackets () are allowed whereas it is flexiable for quotes both single quotes '', double quotes "" are acceptable with only condition being both opening and ending quote be present to execute the code without error.
+
